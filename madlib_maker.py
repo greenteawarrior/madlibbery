bb7cc236469f92c352e7f2d2301e7dd0cbf30e23
Emily Wang <emily.wang@students.olin.edu>
2013-04-16 21:17:09 -0400

    worked on blankage helper function and used nltk pos_tag

--- madlib_maker.py
+++ madlib_maker.py
@@ -1,4 +1,5 @@
 import random
+import nltk
 
 #emilywang, nicolerifkin, mauracosman
 #softdes!
@@ -11,11 +12,11 @@ def blankage(inputstory_list):
 	#user can decide either random or "every nth number" blankage
 	howtoblank= False #initial value-ing
 	while howtoblank == False:
-		try_rand = input("Do you want to have the madlib blanks spaced randomly throughout the text sample? Press 'y'/'n' and hit enter.")
-		if try_rand == 'y':
+		try_rand = raw_input("Do you want to have the madlib blanks spaced randomly throughout the text sample? Press 'y'/'n' and hit enter.")
+		if str(try_rand) == 'y':
 			howtoblank = 'random'
 		elif try_rand == 'n':
-			try_int = input ("Would you rather have the madlib blanks placed every nth word? If so, how often? (specify a positive integer value) If not, press enter.")
+			try_int = raw_input ("Would you rather have the madlib blanks placed every nth word? If so, how often? (specify a positive integer value) If not, press enter.")
 			try:
 				integer = int(try_int)
 				howtoblank = integer	
@@ -23,34 +24,39 @@ def blankage(inputstory_list):
 				howtoblank = False
 		
 	blank = '_____' #this is a string of five underscores
-	blankedoutwords_dict = {}on
+	blankedoutwords_dict = {}
+	tagged = nltk.pos_tag(inputstory_list)
+	blankable_poslist = ['NN', 'VBD', 'VBP', 'JJ', 'RB']
 	if howtoblank == 'random': #we'll blank randomly
 		totalblanks = len(inputstory_list) / 5.0 #total amount of blanks desired
 		howmanyblanks = 0 #counter for current amount of blanks in madlib in progress
-		while howmanyblanks != totalblanks:
+		while howmanyblanks <= totalblanks:
 			wordtoblank = random.choice(inputstory_list)
 			wordtoblank_index = inputstory_list.index(wordtoblank)
-			wordtoblank_pos = #nltk stuff
-				if wordtoblank_pos != #verb or noun or adj:
-					continue
-				else:
-					howmanyblanks = howmanyblanks + 1
-					blankedoutwords_dict[wordtoblank] = '' #look up the part of speech here
-					inputstory_list[wordtoblank_index] = blank
+			wordtoblank_pos = tagged[wordtoblank_index][1]#nltk stuff
+			if wordtoblank_pos in blankable_poslist:
+				howmanyblanks = howmanyblanks + 1
+				blankedoutwords_dict[wordtoblank] = (wordtoblank_index,wordtoblank_pos) #look up the part of speech here
+				inputstory_list[wordtoblank_index] = blank
+		return inputstory_list, blankedoutwords_dict
+			
 	
 	elif howtoblank != 'random' and type(howtoblank)==int: #if it's not random, we want to replace every nth word with a blank; n=howtoblank 
 		wordtoblank_index = howtoblank #initialize
+		howmanyblanks = 0
 		while wordtoblank_index%howtoblank != len(inputstory_list)%howtoblank:
 			wordtoblank = inputstory_list[wordtoblank_index]
-			wordtoblank_pos = #nltk stuff
-				if wordtoblank_pos != #verb or noun or adj:
-					######## what you want to do elsewise
-				else:
-					blankedoutwords_dict[wordtoblank] = '' #look up the part of speech here
-					inputstory_list[wordtoblank_index] = blank
-		print (inputstory_list)
+			wordtoblank_pos = tagged[wordtoblank_index][1]
+			if wordtoblank_pos in blankable_poslist:
+				howmanyblanks = howmanyblanks + 1
+				print (howmanyblanks)
+				blankedoutwords_dict[wordtoblank] = (wordtoblank_index,wordtoblank_pos) #look up the part of speech here
+				inputstory_list[wordtoblank_index] = blank
+				#increment wordtoblank_index
+		return inputstory_list, blankedoutwords_dict
+
 	else:
-	 	return False
+	  	return False
 
 def madlibbing():
 	welcome = "Let's get down to business (to defeat the ___). Compose something and we'll turn it into a madlib. Results may vary (that's the point)."
@@ -59,7 +65,7 @@ def madlibbing():
 	letsbegin = 0
 
 	while letsbegin != 'y':
-		letsbegin = input("If you would like to proceed, press 'y' and then the enter key. ")
+		letsbegin = raw_input("If you would like to proceed, press 'y' and then the enter key. ")
 
 	#whether you compose it yourself or scrape the text from a URL, the inputstory should be a string of sorts
 	#inputstory = str(input("Write stuff here: "))
@@ -72,7 +78,8 @@ def madlibbing():
 
 	inputstory_list = inputstory.split()
 
-	blankage(inputstory_list)
+	x = blankage(inputstory_list)
+	print(x)
 
 	# while blanking == False:
 	#  	blanking = blankage(inputstory, howtoblank)
