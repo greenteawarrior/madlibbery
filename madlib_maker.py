c71fa31a46fd2f525e566368abe1558dfb8eb0c2
Emily Wang <emily.wang@students.olin.edu>
2013-04-18 17:10:26 -0400

    started playage - filling in the blanks by user input

--- madlib_maker.py
+++ madlib_maker.py
@@ -7,77 +7,64 @@ from bs4 import BeautifulSoup
 #softdes!
 #madlib generator
 
-#Next task: finish blankage. Should incorporate a blankedword dictionary...where the key is the blanked word and the value is the part of speech. #Also, a catch for particles/notmadlibfriendly words in the blankedword dictionary.
-
+#If they don't want to play, let them say no. (i.e. letsmake and letsbegin)
+#make sure they can't ask for more madlibs than possible
 def blankage(inputstory_list):
 	#make while loops later to make this program typo-proof o.o
-	#user can decide either random or "every nth number" blankage
 	howtoblank= False #initial value-ing
 	while howtoblank == False:
 		totalblanks = raw_input("How many blanks would you like in your madlib?")
 		try:
 			totalblanks = int(totalblanks)
-			howtoblank = 'random'	
+			howtoblank = True	
 		except:
 			howtoblank = False
 		
 	blank = '_____' #this is a string of five underscores
-	blankedoutwords_dict = {}
+	blank_dict = {}
 	tagged = nltk.pos_tag(inputstory_list)
 
 	blankable_poslist = ['NN', 'VB', 'VBP', 'JJ', 'RB']
-	if howtoblank == 'random': #we'll blank randomly
-		totalblanks = len(inputstory_list) / 5.0 #total amount of blanks desired
-		howmanyblanks = 0 #counter for current amount of blanks in madlib in progress
-		while howmanyblanks <= totalblanks:
-			wordtoblank = random.choice(inputstory_list)
-			wordtoblank_index = inputstory_list.index(wordtoblank)
-			wordtoblank_pos = tagged[wordtoblank_index][1]#nltk stuff
-			if wordtoblank_pos in blankable_poslist and wordtoblank != blank:
-				howmanyblanks = howmanyblanks + 1
-				blankedoutwords_dict[wordtoblank] = (wordtoblank_index,wordtoblank_pos) #look up the part of speech here
-				inputstory_list[wordtoblank_index] = blank
-		return inputstory_list, blankedoutwords_dict
+	howmanyblanks = 0 #counter for current amount of blanks in madlib in progress
 	
-	elif howtoblank != 'random' and type(howtoblank)==int: #if it's not random, we want to replace every nth word with a blank; n=howtoblank 
-		#wordtoblank_index = howtoblank #initialize
-		totalblanks = howtoblank
-		howmanyblanks = 0 #counter
-		while howmanyblanks <= totalblanks:
-			wordtoblank = random.choice(inputstory_list)
-			wordtoblank_index = inputstory_list.index(wordtoblank)
-			wordtoblank_pos = tagged[wordtoblank_index][1]#nltk stuff
-			if wordtoblank_pos in blankable_poslist:
-				howmanyblanks = howmanyblanks + 1
-				blankedoutwords_dict[wordtoblank] = (wordtoblank_index,wordtoblank_pos) #look up the part of speech here
-				inputstory_list[wordtoblank_index] = blank
-		return inputstory_list, blankedoutwords_dict
+	while howmanyblanks < totalblanks:
+		wordtoblank = random.choice(inputstory_list)
+		wordtoblank_index = inputstory_list.index(wordtoblank)
+		wordtoblank_pos = tagged[wordtoblank_index][1]#nltk stuff
+		if wordtoblank_pos in blankable_poslist and wordtoblank != blank:
+			howmanyblanks = howmanyblanks + 1
+			blank_dict[wordtoblank] = (wordtoblank_index,wordtoblank_pos) #look up the part of speech here
+			inputstory_list[wordtoblank_index] = blank
+	return inputstory_list, blank_dict
 
-	else:
-	  	return False
+def playage(madlib):
+	#unpacking the madlib
+	madlib_list = madlib[0]
+	blank_dict = madlib[1]
+	pos_dict= {'NN': 'Noun', 'VB': 'Verb', 'VBP': 'Present Tense Verb', 'JJ': 'Adjective', 'RB': 'Adverb'}
+	print("Reach deep down into your soul and tell me a...")
+	for key in blank_dict:
+		key_pos=pos_dict[blank_dict[key][1]]
+		fillintheblank=raw_input(key_pos + ': ')
+		madlib_list[blank_dict[key][0]]=fillintheblank
+	print(madlib_list)
+	print(blank_dict)
 
 def madlibbing():
-	welcome = "Let's get down to business (to defeat the ___). Compose something and we'll turn it into a madlib. Results may vary (that's the point)."
-	print (welcome)
+	make_welcome = "Let's get down to business (to defeat the ___). Compose something and we'll turn it into a madlib. Results may vary (that's the point)."
+	print (make_welcome)
 
-	letsbegin = 0
+	letsmake = 0
 
-	while letsbegin != 'y':
-		letsbegin = raw_input("If you would like to proceed, press 'y' and then the enter key. ")
+	while letsmake != 'y':
+		letsmake = raw_input("If you would like to proceed, press 'y' and then the enter key. ")
 
-	#whether you compose it yourself or scrape the text from a URL, the inputstory should be a string of sorts
-	#inputstory = str(input("Write stuff here: "))
-
-	#our test inputstory, by maura
-	#write your own madlib
-	#take text from a url
-	#Use a pre-written story
 	print("Would you like to use one of our Madlib stories, choose one from elsewhere, or create your own?")
 	inputstory = False
 	while inputstory == False:
 		inputstorytype = raw_input("Type 'yours' to use a story we've written. Type 'mine' to write or paste a story yourself. Type 'url' to turn the text from a website URL into a madlib")
 		if inputstorytype == 'yours':
-			inputstory = 'There was once a strange creature wandering around Olin. It had the head of a cat and the body of an octopus. I decided to call it Octocat.'
+			inputstory = 'There was a strange creature wandering around Olin. It had the head of a cat and the body of an octopus. I decided to call it Octocat.'
 			print(inputstory)
 		elif inputstorytype == 'mine':
 			inputstory = raw_input("Type or paste your story here") #check to make sure that entry is valid
@@ -94,17 +81,24 @@ def madlibbing():
 				print("That is not a valid url.")
 				inputstory = False
 
+	inputstory_list = inputstory.split() 
 	#turn the inputstory into a list, where each element is a word. this isn't very friendly to spacebar typos.
 	#we chose a list rather than a dictionary because even though dictionaries are faster, we need the elements to be sequential.
 
-	inputstory_list = inputstory.split()
+	madlib = False
+	while madlib == False:
+		madlib = blankage(inputstory_list)
 
-	x = blankage(inputstory_list)
-	print(x)
+	print("Your madlib is now complete (freshly out of the oven and smells ______, if I might add). Would you like to play?")
 
-	# while blanking == False:
-	#  	blanking = blankage(inputstory, howtoblank)
+	letsplay = 0
 
-	# return
+	while letsplay != 'y':
+		letsplay = raw_input("If you would like to proceed, press 'y' and then the enter key. ")
+
+	playing = False
+	while playing == False:
+		playing = playage(madlib)
+
 
 madlibbing()
