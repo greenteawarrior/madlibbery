163c946f958a42a026c1cc89bd5d0b6136b8e407
rrifkin <rifkinni@ubuntu.(none)>
2013-05-09 21:40:17 -0400

    Final final final

--- testing.py
+++
@@ -1,9 +0,0 @@
-import timeit
-p = timeit.default_timer()
-q = timeit.default_timer()
-#for i in range(10000):
-while q - p < 5:
-	q = timeit.default_timer()
-	#if q - p > 15:
-	#	break
-print("Would you like inspiration?")
